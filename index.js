5029996eff988864bfc3c923f7f3d250347a47ed
Faisal Nour <faisalskhalifa@gmail.com>
2021-02-20 23:29:52 -0500

    comments are added to index.js file and unnecessary console logs are removed

--- index.js
+++ index.js
@@ -12,48 +12,42 @@ app.use(express.json());
 
 let savedNoteList = './.noteList.json';
 
-
-
-// let noteList = [{ id:0, noteTitle: 'Go to the store', noteText: 'I need to go there and do something' }, {id:5, noteTitle: 'Go to the bank', noteText: 'Request a new cheque book' }, {id:2, noteTitle: 'Go to the doctor', noteText: 'Need to get checkup your blood pressure' }, {id:8, noteTitle: 'Go to visit Grandma', noteText: 'Go to the hospital and visit your grandmother' }];
-
 let noteList = fs.existsSync(savedNoteList) ?
 JSON.parse( fs.readFileSync(savedNoteList) ) : []
 
-console.log(noteList)
 
+// function to send all notes saved when api is called
 app.get('/api/notes', function (req, res) {
     res.send(noteList);
 });
 
 
-var getIndex;
-// review this tomorrow and fix it, just run it becuse I am close to fix it
+// function to send a specifc element by passing noteID 
 app.get('/api/:id', function(req,res){
     const targetedDisplayID = req.params.id
-    console.log(`value of targetedDisplayID =  ${targetedDisplayID}`)
-    console.log("type of targetedDisplayID" + typeof(targetedDisplayID))
+    // console.log(`value of targetedDisplayID =  ${targetedDisplayID}`)
+    // console.log("type of targetedDisplayID" + typeof(targetedDisplayID))
     
+    var getIndex;
+
     for(i=0; i<noteList.length; i++){
         if(noteList[i].id === JSON.parse(targetedDisplayID)){
-        //  if(noteList[i].id === targetedDisplayID){
-            console.log("found it")
-            console.log(`i = ${i}`)
             getIndex = i;
-            console.log(`getIndex = ${getIndex}`)
-            // to send it to the variable on html file 
+            // console.log(`getIndex = ${getIndex}`)
         }
     }  
-    console.log(`the clicked element has index: ${JSON.stringify(noteList[getIndex])}`)
+    // console.log(`the clicked element has index: ${JSON.stringify(noteList[getIndex])}`)
 
-    // NA clear noteList and get a new version of it 
+    // Clear noteList and get latest version of it 
     noteList = [];
     noteList =  JSON.parse( fs.readFileSync(savedNoteList))
 
     // to send it to the variable on html file 
     res.send(noteList[getIndex])
-    //console.log(noteList[1])
 })
 
+
+// to delete a note by passing noteID
 app.post('/api/notes/delete', function (req, res) {
     console.log('table id req.body: ', req.body.id);
     const deleteTableId = req.body.id;
@@ -63,23 +57,13 @@ app.post('/api/notes/delete', function (req, res) {
             targetedIndex = j;
         }
     }
-    // add this now
-    // const newNoteData =  noteList.splice(deleteTableId,1)
+    
     noteList.splice(targetedIndex,1)
     fs.writeFileSync( savedNoteList, JSON.stringify( noteList ) )
-    // add this now, to clear the existing data
-    // noteList = []
-    // add this now,
-    // noteList = newNoteData
-    // add this now
-    // fs.writeFileSync( noteList, JSON.stringify( noteList ) )
-    console.log(noteList)
-    // const newTableData = req.body;
-    // tableList.push(newTableData)
-    // console.log(tableList)
-    // res.send({message: `done`})
-}); // end of post api/notes/delete
+    
+}); 
 
+// to add new note 
 app.post('/api/note/add', function(req,res){
     
     let newNote = req.body;
@@ -92,11 +76,8 @@ app.post('/api/note/add', function(req,res){
 
      noteList.push(newNote)
      fs.writeFileSync( savedNoteList, JSON.stringify( noteList ) )
-
-
-
-
 })
+
 
 app.listen(PORT, function () {
     console.log('http://localhost:' + PORT);
